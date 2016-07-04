a91d7505bc6a27fab2ed94fd510ba2876b221eae
ZhuXiaolei <xiaolei@ZhuXiaoleide-MacBook-Pro.local>
2016-07-04 23:20:10 +0800

    fix bugs

--- HelloTriangles/main.cpp
+++ HelloTriangles/main.cpp
@@ -42,19 +42,24 @@ int main(int argc, const char * argv[]) {
 	GLfloat vertices[]={
 		-0.5f, -0.5f, 0.0f,
 		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
+		0.0f, 0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        1.0f, 0.5f, 0.0f
 	};
 
 	GLuint VBO, VAO;
 	glGenBuffers(1, &VBO);
 	glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
     
+    // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+    glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
+    
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
     
@@ -62,6 +67,8 @@ int main(int argc, const char * argv[]) {
     const GLchar* frag = "/Users/xiaolei/Develop/GitHub/LearnOpenGL/HelloTriangles/triangles.frag";
 	Shader shader(vert, frag);
 
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    
 	while (!glfwWindowShouldClose(window))
     {
         // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
@@ -72,7 +79,7 @@ int main(int argc, const char * argv[]) {
 
     	shader.Use();
     	glBindVertexArray(VAO);
-    	glDrawArrays(GL_TRIANGLES, 0, 3);
+    	glDrawArrays(GL_TRIANGLES, 0, 6);
     	glBindVertexArray(0);
         
         // Swap the screen buffers
